59740d0556f89a77aab1ea97f9dff1d7df069f50
anirudhbhalekar <98670730+anirudhbhalekar@users.noreply.github.com>
2022-11-07 11:55:30 +0000

    Upload

---
+++ un_distort.py
@@ -0,0 +1,21 @@
+import cv2
+import numpy as np
+import sys
+import glob
+
+filename = 'orientatio'
+DIM=(1016, 760)
+K= np.array([[567.4130565572482, 0.0, 501.39791714355], [0.0, 567.3325405728447, 412.9039077874256], [0.0, 0.0, 1.0]])
+D=np.array([[-0.05470334257497442], [-0.09142371384400942], [0.17966906821072895], [-0.08708720575337928]])
+
+def undistort(img_path):
+    img = cv2.imread(img_path)
+    h,w = img.shape[:2]
+    map1, map2 = cv2.fisheye.initUndistortRectifyMap(K, D, np.eye(3), K, DIM, cv2.CV_16SC2)
+    undistorted_img = cv2.remap(img, map1, map2, interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT)
+    cv2.imshow("undistorted", undistorted_img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+if __name__ == '__main__':
+    for image in glob.glob("{}/*.jpg".format(filename)):
+        undistort(image)
