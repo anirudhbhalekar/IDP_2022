7d44fcda0b0f90888a2815d13fdacd4c145f5348
George Hogg <GeorgeSHogg@outlook.com>
2022-11-08 09:53:50 +0000

    upload of detection algorithms

---
+++ colour_detection.py
@@ -0,0 +1,40 @@
+import cv2 
+import time 
+import matplotlib.pyplot as plt
+import os
+import numpy as np 
+
+img_path = "1_pink_arrow.jpg"
+
+img = cv2.imread(img_path)
+
+DIM=(1016, 760)
+K=np.array([[567.4130565572482, 0.0, 501.39791714355], [0.0, 567.3325405728447, 412.9039077874256], [0.0, 0.0, 1.0]])
+D=np.array([[-0.05470334257497442], [-0.09142371384400942], [0.17966906821072895], [-0.08708720575337928]])
+
+h,w = img.shape[:2]
+map1, map2 = cv2.fisheye.initUndistortRectifyMap(K, D, np.eye(3), K, DIM, cv2.CV_16SC2)
+undistorted_img = cv2.remap(img, map1, map2, interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT)
+
+hsv = cv2.cvtColor(undistorted_img, cv2.COLOR_BGR2HSV)
+
+print(hsv[518, 750])
+
+#green
+upper = np.array([31, 270, 255])
+lower = np.array([29, 90,	0])
+
+#pink
+#upper = np.array([170, 180, 255])
+#lower = np.array([150, 100,	210])
+
+mask = cv2.inRange(hsv, lower, upper)
+res = cv2.bitwise_and(undistorted_img, undistorted_img, mask= mask)
+
+cv2.imshow('frame', undistorted_img)
+cv2.waitKey(0)
+cv2.imshow('mask',mask)
+cv2.waitKey(0)
+cv2.imshow('res',res)
+cv2.waitKey(0)
+#cv2.imwrite("green1.jpg", mask)
---
+++ object_detection.py
@@ -0,0 +1,114 @@
+import cv2
+from math import atan2, cos, sin, sqrt, pi
+import numpy as np
+
+DIM=(1016, 760)
+K= np.array([[567.4130565572482, 0.0, 501.39791714355], [0.0, 567.3325405728447, 412.9039077874256], [0.0, 0.0, 1.0]])
+D=np.array([[-0.05470334257497442], [-0.09142371384400942], [0.17966906821072895], [-0.08708720575337928]])
+
+def undistort(img_path):
+    img = cv2.imread(img_path)
+    map1, map2 = cv2.fisheye.initUndistortRectifyMap(K, D, np.eye(3), K, DIM, cv2.CV_16SC2)
+    undistorted_img = cv2.remap(img, map1, map2, interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT)
+    return undistorted_img
+
+def drawAxis(img, p_, q_, color, scale):
+  p = list(p_)
+  q = list(q_)
+ 
+  ## [visualization1]
+  angle = atan2(p[1] - q[1], p[0] - q[0]) # angle in radians
+  hypotenuse = sqrt((p[1] - q[1]) * (p[1] - q[1]) + (p[0] - q[0]) * (p[0] - q[0]))
+ 
+  # Here we lengthen the arrow by a factor of scale
+  q[0] = p[0] - scale * hypotenuse * cos(angle)
+  q[1] = p[1] - scale * hypotenuse * sin(angle)
+  cv2.line(img, (int(p[0]), int(p[1])), (int(q[0]), int(q[1])), color, 3, cv2.LINE_AA)
+ 
+  # create the arrow hooks
+  p[0] = q[0] + 9 * cos(angle + pi / 4)
+  p[1] = q[1] + 9 * sin(angle + pi / 4)
+  cv2.line(img, (int(p[0]), int(p[1])), (int(q[0]), int(q[1])), color, 3, cv2.LINE_AA)
+ 
+  p[0] = q[0] + 9 * cos(angle - pi / 4)
+  p[1] = q[1] + 9 * sin(angle - pi / 4)
+  cv2.line(img, (int(p[0]), int(p[1])), (int(q[0]), int(q[1])), color, 3, cv2.LINE_AA)
+  ## [visualization1]
+ 
+def getOrientation(pts, img):
+  ## [pca]
+  # Construct a buffer used by the pca analysis
+  sz = len(pts)
+  data_pts = np.empty((sz, 2), dtype=np.float64)
+  for i in range(data_pts.shape[0]):
+    data_pts[i,0] = pts[i,0,0]
+    data_pts[i,1] = pts[i,0,1]
+ 
+  # Perform PCA analysis
+  mean = np.empty((0))
+  mean, eigenvectors, eigenvalues = cv2.PCACompute2(data_pts, mean)
+ 
+  # Store the center of the object
+  cntr = (int(mean[0,0]), int(mean[0,1]))
+  ## [pca]
+ 
+  ## [visualization]
+  # Draw the principal components
+  cv2.circle(img, cntr, 3, (255, 0, 255), 2)
+  p1 = (cntr[0] + 0.02 * eigenvectors[0,0] * eigenvalues[0,0], cntr[1] + 0.02 * eigenvectors[0,1] * eigenvalues[0,0])
+  p2 = (cntr[0] - 0.02 * eigenvectors[1,0] * eigenvalues[1,0], cntr[1] - 0.02 * eigenvectors[1,1] * eigenvalues[1,0])
+  drawAxis(img, cntr, p1, (255, 255, 0), 1)
+  drawAxis(img, cntr, p2, (0, 0, 255), 5)
+ 
+  angle = atan2(eigenvectors[0,1], eigenvectors[0,0]) # orientation in radians
+  ## [visualization]
+ 
+  # Label with the rotation angle
+  label = "  Rotation Angle: " + str(-int(np.rad2deg(angle)) - 90) + " degrees"
+  textbox = cv2.rectangle(img, (cntr[0], cntr[1]-25), (cntr[0] + 250, cntr[1] + 10), (255,255,255), -1)
+  cv2.putText(img, label, (cntr[0], cntr[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
+ 
+  return angle
+ 
+# Load the image
+#img = cv2.imread("framecollection\9_IDP_TEST.jpg")
+file_name = "green1.jpg"
+#img = cv2.imread("white.jpg")
+img = undistort(file_name)
+# Was the image there?
+if img is None:
+  print("Error: File not found")
+  exit(0)
+ 
+cv2.imshow('Input Image', img)
+ 
+# Convert image to grayscale
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+ 
+# Convert image to binary
+_, bw = cv2.threshold(gray, 50, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+ 
+# Find all the contours in the thresholded image
+contours, _ = cv2.findContours(bw, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+ 
+for i, c in enumerate(contours):
+ 
+  # Calculate the area of each contour
+  area = cv2.contourArea(c)
+ 
+  # Ignore contours that are too small or too large
+  if area < 3: #or 100000 < area:
+    continue
+ 
+  # Draw each contour only for visualisation purposes
+  cv2.drawContours(img, contours, i, (0, 0, 255), 2)
+ 
+  # Find the orientation of each shape
+  getOrientation(c, img)
+ 
+cv2.imshow('Output Image', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+  
+# Save the output image to the current directory
+#cv.imwrite("output_img.jpg", img)
