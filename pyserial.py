2abd1aa2cfc9ebc45d212586bd2a82f37e5a7159
anirudhbhalekar <anirudhbhalekar10@gmail.com>
2022-11-15 10:58:46 +0000

    pyserial edits

--- pyserial.py
+++ pyserial.py
@@ -1,6 +1,7 @@
 import time
 import serial
 
+
 # configure the serial connections (the parameters differs on the device you are connecting to)
 ser = serial.Serial(
     port='COM9',
@@ -11,6 +12,12 @@ ser = serial.Serial(
 )
 
 ser.isOpen()
+ser.close()
+ser.open()
+
+timeout = ser.timeout
+ser.timeout = 2
+
 
 print('Enter your commands below.\r\nInsert "exit" to leave the application.')
 
@@ -20,13 +27,15 @@ while 1 :
     data_in = input(">> ")
     if data_in == 'exit':
         ser.close()
+        ser.timeout = timeout
         exit()
     else:
+        _ = ser.read_all()
         # send the character to the device
         # (note that I happend a \r\n carriage return and line feed to the characters - this is requested by my device)
         serial_data = bytes(str(data_in), encoding='utf8')
         ser.write(serial_data)
-        out = ''
+        out = ser.read_until(  )
         # let's wait one second before reading output (let's give device time to answer)
         #time.sleep(1)
         #while ser.inWaiting() > 0:
