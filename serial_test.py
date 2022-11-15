07346aca528dd200ff25a25c43a2112334b3cb6b
anirudhbhalekar <anirudhbhalekar10@gmail.com>
2022-11-15 02:40:50 +0000

    serial test

--- serial_test.py
+++ serial_test.py
@@ -1,3 +1,19 @@
-import serial 
-import time 
+# Importing Libraries
 
+import serial
+import time
+
+# The port by default set by BLE initialisation is COM9
+
+arduino = serial.Serial(port='COM9', baudrate=115200, timeout=.1)
+
+def write_read(x):
+    arduino.write(bytes(x, 'utf-8'))
+    time.sleep(0.5)
+    data = arduino.readline()
+    return data
+
+while True:
+    num = input("Enter a number: ") # Taking input from user
+    value = write_read(num)
+    print(value) # printing the value
