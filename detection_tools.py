e4a3d2e386d41d5a3c4fc6c0b4489d0a276a3c39
anirudhbhalekar <anirudhbhalekar10@gmail.com>
2022-11-22 12:53:25 +0000

    added aruco detection to detection tools

--- detection_tools.py
+++ detection_tools.py
@@ -7,6 +7,106 @@ DIM = (1016, 760)
 K = np.array([[567.4130565572482, 0.0, 501.39791714355], [0.0, 567.3325405728447, 412.9039077874256], [0.0, 0.0, 1.0]])
 D = np.array([[-0.05470334257497442], [-0.09142371384400942], [0.17966906821072895], [-0.08708720575337928]])
 
+
+ARUCO_DICT = {
+	"DICT_4X4_50": cv2.aruco.DICT_4X4_50,
+	"DICT_4X4_100": cv2.aruco.DICT_4X4_100,
+	"DICT_4X4_250": cv2.aruco.DICT_4X4_250,
+	"DICT_4X4_1000": cv2.aruco.DICT_4X4_1000,
+	"DICT_5X5_50": cv2.aruco.DICT_5X5_50,
+	"DICT_5X5_100": cv2.aruco.DICT_5X5_100,
+	"DICT_5X5_250": cv2.aruco.DICT_5X5_250,
+	"DICT_5X5_1000": cv2.aruco.DICT_5X5_1000,
+	"DICT_6X6_50": cv2.aruco.DICT_6X6_50,
+	"DICT_6X6_100": cv2.aruco.DICT_6X6_100,
+	"DICT_6X6_250": cv2.aruco.DICT_6X6_250,
+	"DICT_6X6_1000": cv2.aruco.DICT_6X6_1000,
+	"DICT_7X7_50": cv2.aruco.DICT_7X7_50,
+	"DICT_7X7_100": cv2.aruco.DICT_7X7_100,
+	"DICT_7X7_250": cv2.aruco.DICT_7X7_250,
+	"DICT_7X7_1000": cv2.aruco.DICT_7X7_1000,
+	"DICT_ARUCO_ORIGINAL": cv2.aruco.DICT_ARUCO_ORIGINAL,
+	"DICT_APRILTAG_16h5": cv2.aruco.DICT_APRILTAG_16h5,
+	"DICT_APRILTAG_25h9": cv2.aruco.DICT_APRILTAG_25h9,
+	"DICT_APRILTAG_36h10": cv2.aruco.DICT_APRILTAG_36h10,
+	"DICT_APRILTAG_36h11": cv2.aruco.DICT_APRILTAG_36h11
+}
+
+
+aruco_type = "DICT_4X4_250"
+
+arucoDict = cv2.aruco.Dictionary_get(ARUCO_DICT[aruco_type])
+
+arucoParams = cv2.aruco.DetectorParameters_create()
+
+def aruco_display(img, corners, ids, rejected, image): 
+    if len(corners) > 0: 
+        print("Detected")
+        ids = ids.flatten()
+
+        for (markerCorner, markerID) in zip(corners, ids):
+            corners = markerCorner.reshape((4,2))
+            (topLeft, topRight, bottomRight, bottomLeft) = corners
+            #print(corners)
+
+            topRight = (int(topRight[0]), int(topRight[1]))
+            bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
+            bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
+            topLeft = (int(topLeft[0]), int(topLeft[1]))
+
+            cv2.line(image, topLeft, topRight, (0,255,0), 3)
+            cv2.line(image, topRight, bottomRight, (0,255,0), 3)
+            cv2.line(image, bottomRight, bottomLeft, (0,255,0), 3)
+            cv2.line(image, bottomLeft, topLeft, (0,255,0), 3)
+
+            topMid = (int((topRight[0] + topLeft[0])/2.0), int((topRight[1] + topLeft[1])/2.0))
+            bottomMid = (int((bottomLeft[0] + bottomRight[0])/2.0), int((bottomLeft[1] + bottomRight[1])/2.0))
+
+            cX = int((topLeft[0] + bottomRight[0])/2.0)
+            cY = int((topLeft[1] + bottomRight[1])/2.0)
+            cv2.circle(image, (cX,cY), 8, (0,0,255), -1)
+
+            cv2.arrowedLine(img, topMid, bottomMid, (255,0,0), 2)
+        
+            #cv2.putText(image, str(angle), (topMid[0], topMid[1] + 100), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0), 2)
+            cv2.putText(image, str(markerID), (topLeft[0], topLeft[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0),2)
+            print("[Inference] ArUco marker ID: {}".format(markerID))
+
+            return image
+
+def aruco_detection(img): 
+
+    x = None
+    corners, ids, rejected = cv2.aruco.detectMarkers(img, arucoDict, parameters=arucoParams)
+    return corners, ids
+
+def return_angle(coords0, coords1): 
+    x0,y0 = coords0[0], coords0[1]
+    x1,y1 = coords1[0], coords1[1]
+
+    angle = (180/np.pi) * np.arctan((y1-y0)/(x1-x0))
+
+    return angle
+
+def get_pose(corners): 
+
+    (topLeft, topRight, bottomRight, bottomLeft) = corners
+
+    topRight = (int(topRight[0]), int(topRight[1]))
+    bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
+    bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
+    topLeft = (int(topLeft[0]), int(topLeft[1]))
+
+    topMid = (int((topRight[0] + topLeft[0])/2.0), int((topRight[1] + topLeft[1])/2.0))
+    bottomMid = (int((bottomLeft[0] + bottomRight[0])/2.0), int((bottomLeft[1] + bottomRight[1])/2.0))
+
+    cX = int((topLeft[0] + bottomRight[0])/2.0)
+    cY = int((topLeft[1] + bottomRight[1])/2.0)
+
+    angle = return_angle(topMid, bottomMid)
+
+    return cX, cY, angle
+    
 def undistort(img):
     #function to remove fisheye from an image taken with the overhead camera
     dim1 = img.shape[:2][::-1]  
@@ -28,28 +128,6 @@ def detect_colour(img, upper, lower, ret_colour = False):
         return mask
     else:
         return np.expand_dims(mask, -1) * img
-
-def sobel(image):
-    #returns sobel filtered image 
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-    grad_x = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=3)
-    grad_y = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=3)
-
-    abs_grad_x = cv2.convertScaleAbs(grad_x)
-    abs_grad_y = cv2.convertScaleAbs(grad_y)
-
-    grad = cv2.addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0)
-    
-    return grad
-
-def edge_detection(img):
-    #returns map of edges of original image
-    grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    dx = cv2.convertScaleAbs(cv2.Sobel(grey, cv2.CV_64F, 1, 0, ksize=3))
-    dy = cv2.convertScaleAbs(cv2.Sobel(grey, cv2.CV_64F, 0, 1, ksize=3))
-    grad = cv2.addWeighted(dx, 0.5, dy, 0.5, 0)
-    return grad
 
 def binary_image(img, cutoff = 50):
     #returns a binary map of the inputed image with a greyscale cutoff as specified
