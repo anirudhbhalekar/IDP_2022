f394acfc0fa83fb0e2578e9a5170cafac4c0dab5
anirudhbhalekar <anirudhbhalekar10@gmail.com>
2022-11-24 10:00:30 +0000

    function for reading ultrasound

--- pyserial.py
+++ pyserial.py
@@ -12,10 +12,14 @@ timeout = ser.timeout
 ser.timeout = 2
 dec_val_list = []
 
-writeMode = 0
+writeMode = 1
 
 print('Enter your commands below.\r\nInsert "exit" to leave the application.')
 data_in = 1
+ser.write(b"20")
+time.sleep(3)
+ser.write(b"21")
+time.sleep(3)
 while 1:
 
     if writeMode == 0: 
@@ -40,20 +44,28 @@ while 1:
         splice_read = str(raw_read)[4:-1]
         
         if len(splice_read) > 0:
-            dec_val = int(splice_read, base=16)
-            dec_val_list.append(dec_val)
+            try: 
+                dec_val = int(splice_read, base=16)
+                dec_val_list.append(dec_val)
+                print(dec_val)
+            except: 
+                pass
+            time.sleep(0.1)
         
         else: 
             pass 
+        
+        print(dec_val_list)
 
         if len(dec_val_list) > 8: 
-            spliced_list = dec_val_list.sort()[0:3]
-            bot_average = sum(spliced_list)/len(spliced_list)
-
-            if bot_average > 15: 
-                print("LOW DENSITY BLOCK")
-            else: 
+            bot_average = sum(dec_val_list)/len(dec_val_list)
+            print(bot_average)
+            if bot_average > 40: 
                 print("HIGH DENSITY BLOCK")
-        time.sleep(1)
+            else: 
+                print("LOW DENSITY BLOCK")
+            break
+
+        time.sleep(0.1)
 
 
